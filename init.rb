285875d482619652e1cb02655f3c7a6ec660c881
johannes <developer2@developer-2s-computer.local>
2008-06-28 22:13:50 +0200

    Refracted the structure. Added CSV support. Added feature to redirect if only one hit.

--- init.rb
+++ init.rb
@@ -1,8 +1,9 @@
 # Include hook code here
 require 'will_paginate'
-ActionController::Base.class_eval { include ListFor::ActionController::InstanceMethods }
+ActionController::Base.class_eval { include ListFor::Extensions::ActionController::InstanceMethods }
 ActionController::Base.before_filter :load_list_params
-ActionController::Base.helper ListForHelper
-TrueClass.class_eval { include ListFor::TrueClass::InstanceMethods }
-FalseClass.class_eval { include ListFor::FalseClass::InstanceMethods }
-NilClass.class_eval { include ListFor::NilClass::InstanceMethods }
+ActionController::Base.helper ListFor::Helper
+Array.class_eval { include ListFor::Extensions::Array::InstanceMethods }
+TrueClass.class_eval { include ListFor::Extensions::TrueClass::InstanceMethods }
+FalseClass.class_eval { include ListFor::Extensions::FalseClass::InstanceMethods }
+NilClass.class_eval { include ListFor::Extensions::NilClass::InstanceMethods }
